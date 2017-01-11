4cbcee2b566562fd6a718eca5f8f4c66261ec418
parag87 <paragmundra@gmail.com>
2017-01-11 10:40:12 +0530

    first commit

---
+++ src/main/java/com/allstate/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.allstate.controllers;
+
+import com.allstate.models.Hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by localadmin on 11/01/17.
+ */
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Hello home(){
+        Hello h = new Hello("Hello Spring");
+        return h;
+    }
+}
